b9fb3543c579248942ac7c48f0c556e010673353
8Yanrin <144094648+8Yanrin@users.noreply.github.com>
2023-09-26 06:59:20 +0000

    test

--- app/8Yan/function_app.py
+++ app/8Yan/function_app.py
@@ -17,7 +17,7 @@ def http_trigger(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully. weeeeeeee")
     else:
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
