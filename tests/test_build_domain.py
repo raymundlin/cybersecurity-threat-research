06c0397f7ca85e33d9764857f01bda8b76ef2f21
Ｌｅｏ <102800624+Leowwd@users.noreply.github.com>
2023-10-03 09:55:05 +0000

    Fixed the package issue and improved

--- tests/test_build_domain.py
+++ tests/test_build_domain.py
@@ -1,4 +1,19 @@
-from src.build_domain import count_yaml_keys
+import sys
+sys.path.append("/workspaces/cybersecurity-threat-research/src")
+
+from build_domain import *
+
+yaml_files = find_yaml_files("/workspaces/cybersecurity-threat-research/domain/study")
+
+
+def test_find_yaml_files():
+    assert find_yaml_files("/workspaces/cybersecurity-threat-research/domain/study") is not None
 
 def test_count_yaml_keys():
-    assert count_yaml_keys != 0
+    for yaml_file_path in yaml_files:
+        assert count_yaml_keys(yaml_file_path) != 0
+
+def test_get_yaml_content():
+    for yaml_file_path in yaml_files:
+        assert get_yaml_content(yaml_file_path) is not None
+
