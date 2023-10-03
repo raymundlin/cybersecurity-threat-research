e0a8671aec16dfd21ee0064130f4ab465999882c
Ｌｅｏ <102800624+Leowwd@users.noreply.github.com>
2023-10-03 17:12:41 +0000

    Forth Commit (I found that deleting deployment can solve issues)

--- tests/test_build_domain.py
+++ tests/test_build_domain.py
@@ -1,20 +1,28 @@
 import sys
 sys.path.append("./")
 
-from build_domain import *
+import build_domain
+import build_statistics
+import build_threat
 
 PATH = "./domain/study"
+PATH2 = "./threat/actor"
 
-yaml_files = find_yaml_files(PATH)
-
-
-def test_find_yaml_files():
-    assert find_yaml_files(PATH) is not None
+yaml_files = build_domain.find_yaml_files(PATH)
+yaml_files2 = build_threat.find_yaml_files(PATH2)
+yaml_files3 = build_statistics.find_yaml_files(PATH)
 
 def test_count_yaml_keys():
     for yaml_file_path in yaml_files:
-        assert count_yaml_keys(yaml_file_path) != 0
+        assert build_domain.count_yaml_keys(yaml_file_path) != 0
+
+    for yaml_file_path in yaml_files2:
+        assert build_threat.count_yaml_keys(yaml_file_path)
 
 def test_get_yaml_content():
     for yaml_file_path in yaml_files:
-        assert get_yaml_content(yaml_file_path) is not None
+        assert build_domain.get_yaml_content(yaml_file_path) is not None
+        assert build_statistics.get_yaml_content(yaml_file_path) is not None
+
+    for yaml_file_path in yaml_files2:
+        assert build_threat.get_yaml_content(yaml_file_path) is not None
