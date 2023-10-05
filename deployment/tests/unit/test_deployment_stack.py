aa948ef122bb1f4560e321e773350151e9db6351
stanma9107 <lol97554@gmail.com>
2023-10-05 11:27:42 -0500

    test: check cdk deployment stack. (#156)

--- deployment/tests/unit/test_deployment_stack.py
+++ deployment/tests/unit/test_deployment_stack.py
@@ -5,11 +5,23 @@ from deployment.deployment_stack import DeploymentStack
 
 # example tests. To run these tests, uncomment this file along with the example
 # resource in deployment/deployment_stack.py
-def test_sqs_queue_created():
+def test_lambda_function_created():
     app = core.App()
-    stack = DeploymentStack(app, "deployment")
+    stack = DeploymentStack(app, "deployment", testing = True)
     template = assertions.Template.from_stack(stack)
 
-#     template.has_resource_properties("AWS::SQS::Queue", {
-#         "VisibilityTimeout": 300
-#     })
+    # Check Lambda Function Exists
+    template.has_resource_properties("AWS::Lambda::Function", {
+        "Handler": "list.main",
+        "Runtime": "python3.9"
+    })
+
+def test_api_gateway_created():
+    app = core.App()
+    stack = DeploymentStack(app, "deployment", testing = True)
+    template = assertions.Template.from_stack(stack)
+
+    # Check API Gateway Exists
+    template.has_resource_properties("AWS::ApiGateway::RestApi", {
+        "Name": "Deployment Service",
+    })
