781b83bbc405d8052e719753eb2cc7151c40203a
John <121889006+jgw0915@users.noreply.github.com>
2023-10-24 07:14:42 +0000

    Add: logging in build_domain.py

--- build_domain.py
+++ build_domain.py
@@ -1,31 +1,46 @@
 """ This py build summary md from yaml files """
 
 import os
+import logging
 
 import pandas as pd
 import yaml
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
 def count_yaml_keys(file_path):
     """count"""
-    with open(file_path, "r", encoding="utf-8") as yaml_file:
-        data = yaml.safe_load(yaml_file)
-        if data is None:
-            return 0
-        return len(data.keys())
+    logging.info('running count_yaml_keys ')
+    try:
+        with open(file_path, "r", encoding="utf-8") as yaml_file:
+            data = yaml.safe_load(yaml_file)
+            if data is None:
+                logging.warning('file is empty')
+                return 0
+            return len(data.keys())
+    except:
+        logging.fatal('Something wrong when open the file path. Please check')
 
 
 def get_yaml_content(file_path):
-    with open(file_path, "r", encoding="utf-8") as yaml_file:
-        return yaml.safe_load(yaml_file)
+    logging.info('running get_yaml_content ')
+    try:
+        with open(file_path, "r", encoding="utf-8") as yaml_file:
+            return yaml.safe_load(yaml_file)
+    except IOError as e:
+        logging.fatal(e)
 
 
 def find_yaml_files(root_dir):
     """process all yamls"""
+    logging.info('running find_yaml_files ')
     yaml_files = []
     for item in os.listdir(root_dir):
         if os.path.isfile(os.path.join(root_dir, item)):
             if item.endswith(".yaml") or item.endswith(".yml"):
-                yaml_files.append(os.path.join(root_dir, item))
+                try :
+                    yaml_files.append(os.path.join(root_dir, item))
+                except TypeError as e:
+                    logging.error(e)
     return yaml_files
 
 
@@ -34,10 +49,10 @@ def main(root_directory):
     vector = {}
     yaml_files = find_yaml_files(root_directory)
 
-    print(yaml_files)
+    logging.debug(yaml_files)
 
     if not yaml_files:
-        print("No YAML files found in the specified directory.")
+        logging.debug("No YAML files found in the specified directory.")
         return
 
     for yaml_file_path in yaml_files:
@@ -61,11 +76,14 @@ def main(root_directory):
     df = pd.DataFrame(rows)
 
     # Write Domain Summary
-    with open(f"{root_directory}/README.md", "w+", encoding="utf-8") as markdownFile:
-        markdownFile.write("### domain\Study\n")
-        markdownFile.write("\n")
-        markdownFile.writelines(df.to_markdown(index=False))
-        print(df)
+    try:
+        with open(f"{root_directory}/README.md", "w+", encoding="utf-8") as markdownFile:
+            markdownFile.write("### domain\Study\n")
+            markdownFile.write("\n")
+            markdownFile.writelines(df.to_markdown(index=False))
+        logging.debug(df)
+    except IOError as e:
+        logging.fatal(e)
 
 if __name__ == "__main__":
     main("./domain/study")
