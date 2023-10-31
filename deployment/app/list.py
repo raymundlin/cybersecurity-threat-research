6785f2969bbe0e26778b500e1183a94127e0e10b
8Yanrin <144094648+8Yanrin@users.noreply.github.com>
2023-10-31 06:52:25 +0000

    test

--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,5 +1,12 @@
 import os
 import json
+import logging
+import emoji
+import random
+
+logging.basicConfig(filename = "mylist.jog" , level = logging.DEBUG)
+emojis_list = list(emoji.EMOJI_DATA.keys())
+
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
@@ -7,7 +14,9 @@ def get_files(folder_name):
     for root, dirs, file_names in os.walk(folder_name):
         for file_name in file_names:
             if file_name.endswith('.yaml') or file_name.endswith('.yml'):
-                files.append(file_name)
+                random_emoji = random.choice(emojis_list)
+                files.append(file_name + random_emoji)
+                logging.info("Emoji is good to use right ?")
     return files
 
 def main(event, lambda_context):
@@ -19,6 +28,7 @@ def main(event, lambda_context):
             if folder not in files:
                 files[folder] = []
             files[folder].append(file)
+
 
     return {
         'statusCode': 200,
