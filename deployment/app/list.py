8a09149b5839c2006625775e0741f4a4a6ab00b2
Ｌｅｏ <102800624+Leowwd@users.noreply.github.com>
2023-10-31 07:15:19 +0000

    refactor:log

--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,25 +1,38 @@
 import os
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(level=logging.DEBUG)
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
     files = []
-    for root, dirs, file_names in os.walk(folder_name):
-        for file_name in file_names:
-            if file_name.endswith('.yaml') or file_name.endswith('.yml'):
-                files.append(file_name)
+    try:
+        for root, dirs, file_names in os.walk(folder_name):
+            for file_name in file_names:
+                if file_name.endswith('.yaml') or file_name.endswith('.yml'):
+                    files.append(file_name)
+    except IOError as e:
+        logger.warning(e)
+        logger.debug(e)
     return files
 
 def main(event, lambda_context):
     """Main function."""
     files = {}
+    logger.info(f"{event} is starting.")
     folders = ["threat/actor", "domain/study"]
-    for folder in folders:
-        for file in get_files(folder):
-            if folder not in files:
-                files[folder] = []
-            files[folder].append(file)
-
+    try:
+        for folder in folders:
+            for file in get_files(folder):
+                if folder not in files:
+                    files[folder] = []
+                files[folder].append(file)
+    except IOError as e:
+        logger.fatal(e)
+        logger.debug(e)
+    logger.info(f"{event} is ending.")
     return {
         'statusCode': 200,
         'body': json.dumps({
