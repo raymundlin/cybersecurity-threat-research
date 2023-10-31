cb772dc7cfa2fe2c3dc15d25a993be5e08d6169d
Willypai <122344833+Willypai@users.noreply.github.com>
2023-10-31 08:16:57 +0000

    B11009049_log_list

--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,15 +1,21 @@
 import os
 import json
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
+    if folder_name is None:
+        logger.error("No dictionary")
+        logger.debug("File not found")
     files = []
     for root, dirs, file_names in os.walk(folder_name):
         for file_name in file_names:
             if file_name.endswith('.yaml') or file_name.endswith('.yml'):
                 files.append(file_name)
     return files
-
 def main(event, lambda_context):
     """Main function."""
     files = {}
@@ -18,6 +24,8 @@ def main(event, lambda_context):
         for file in get_files(folder):
             if folder not in files:
                 files[folder] = []
+                logger.fatal("File not found")
+                logger.debug("File not found")
             files[folder].append(file)
 
     return {
