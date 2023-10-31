bdc3c75c88939c8dd138d82c0ac819688a734f51
addfishxu3 <108409983+addfishxu3@users.noreply.github.com>
2023-10-31 19:55:14 +0800

    Update list.py
    Signed-off-by: addfishxu3 <108409983+addfishxu3@users.noreply.github.com>

--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,5 +1,8 @@
 import os
 import json
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
@@ -8,10 +11,12 @@ def get_files(folder_name):
         for file_name in file_names:
             if file_name.endswith('.yaml') or file_name.endswith('.yml'):
                 files.append(file_name)
+    logger.info('Get files from %s',folder_name)
     return files
 
 def main(event, lambda_context):
     """Main function."""
+    logger.info("-Start-")
     files = {}
     folders = ["threat/actor", "domain/study"]
     for folder in folders:
@@ -19,7 +24,8 @@ def main(event, lambda_context):
             if folder not in files:
                 files[folder] = []
             files[folder].append(file)
-
+    logger.info("Finished getting files")
+    logger.info("-End-")
     return {
         'statusCode': 200,
         'body': json.dumps({
