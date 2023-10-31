32e0b5c385d2ffcf91b8cf1b1462b97fc429b43a
rebeccaswc <144219237+rebeccaswc@users.noreply.github.com>
2023-10-31 07:15:34 +0000

    factor: add log to list.py

--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,13 +1,18 @@
 import os
 import json
+import logging
+logging.basicConfig(filename = 'example.log' , encoding = 'utf-8' ,level = logging.DEBUG)
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
     files = []
     for root, dirs, file_names in os.walk(folder_name):
+
         for file_name in file_names:
             if file_name.endswith('.yaml') or file_name.endswith('.yml'):
                 files.append(file_name)
+            else:
+                logging.error("There's no yaml or yml file !!")
     return files
 
 def main(event, lambda_context):
@@ -15,11 +20,14 @@ def main(event, lambda_context):
     files = {}
     folders = ["threat/actor", "domain/study"]
     for folder in folders:
+        logging.info('Srart getting file in {}'.format(folder))
         for file in get_files(folder):
             if folder not in files:
                 files[folder] = []
             files[folder].append(file)
-
+            looging.debug('Appending a file {} to list.'.format(file))
+            logging.info('Finish getting file in {}'.format(folder))
+    looging.info('Return the data {}'.format(data))
     return {
         'statusCode': 200,
         'body': json.dumps({
