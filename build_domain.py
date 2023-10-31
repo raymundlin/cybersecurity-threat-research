800ca649acb1e18468fa6f32ba925c17517f0e45
H1s0kaaa <144766512+H1s0kaaa@users.noreply.github.com>
2023-10-31 06:35:57 +0000

    change on list & add log in buildDomain and list

--- build_domain.py
+++ build_domain.py
@@ -7,6 +7,9 @@ import yaml
 import logging
 
 logging.basicConfig(filename='build_domain.log', encoding='utf-8', level=logging.DEBUG)
+logging.info("Start")
+logging.warning("You have to make sure there is at least one yaml or yml file")
+
 
 def count_yaml_keys(file_path):
     """count"""
@@ -35,6 +38,8 @@ def find_yaml_files(root_dir):
         if os.path.isfile(os.path.join(root_dir, item)):
             if item.endswith(".yaml") or item.endswith(".yml"):
                 yaml_files.append(os.path.join(root_dir, item))
+            else:
+                logging.error("There's no yaml or yml file.")
     return yaml_files
 
 
--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,5 +1,10 @@
 import os
 import json
+import logging
+
+logging.basicConfig(filename='list.log', level=logging.DEBUG)
+logging.info("Start")
+logging.warning("You have to make sure there is at least one yaml or yml file")
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
@@ -8,17 +13,22 @@ def get_files(folder_name):
         for file_name in file_names:
             if file_name.endswith('.yaml') or file_name.endswith('.yml'):
                 files.append(file_name)
+            else:
+                logging.error("There's no yaml or yml file.")
     return files
 
 def main(event, lambda_context):
     """Main function."""
+    logging.debug("Executing Main")
     files = {}
-    folders = ["threat/actor", "domain/study"]
+    folders = ["threat/actor", "domain/study", "backup"]
     for folder in folders:
         for file in get_files(folder):
             if folder not in files:
                 files[folder] = []
             files[folder].append(file)
+
+    logging.info("Finished")
 
     return {
         'statusCode': 200,
