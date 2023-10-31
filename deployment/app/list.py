9b213359564ce2660b878cc9274ffcc73a5eb9e8
ClC <144094751+b11009061@users.noreply.github.com>
2023-10-31 07:34:21 +0000

    clc_list_log

--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,5 +1,9 @@
 import os
 import json
+import logging
+
+logger = logging.getLogger()
+logging.basicConfig(filename = 'clc.log', level = logging.DEBUG)
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
@@ -8,18 +12,22 @@ def get_files(folder_name):
         for file_name in file_names:
             if file_name.endswith('.yaml') or file_name.endswith('.yml'):
                 files.append(file_name)
+            else:
+                logger.error("Your extension need to be .yaml or .yml")
     return files
 
 def main(event, lambda_context):
     """Main function."""
+    logger.info("Start")
     files = {}
     folders = ["threat/actor", "domain/study"]
     for folder in folders:
         for file in get_files(folder):
             if folder not in files:
                 files[folder] = []
+                logger.debug("Your fill can't be null")
             files[folder].append(file)
-
+    logger.info("Ending")
     return {
         'statusCode': 200,
         'body': json.dumps({
