e1b044828103e0df3b653492725f2fca0293c901
sinnyu <144094777+sinnyu@users.noreply.github.com>
2023-10-31 06:51:26 +0000

    feat:deploy_log

--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,14 +1,17 @@
 import os
 import json
+import logging
+
+logging.basicConfig(filename='sinyu.log', encoding='utf-8',level=logging.DEBUG)
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
     files = []
     for root, dirs, file_names in os.walk(folder_name):
         for file_name in file_names:
-            if file_name.endswith('.yaml') or file_name.endswith('.yml'):
+            if file_name.endswith('.yaml') or file_name.endswith('.yml')or file_name.endswith('.png')or file_name.endswith('.PNG'):
                 files.append(file_name)
-    return files
+    return files 
 
 def main(event, lambda_context):
     """Main function."""
@@ -19,10 +22,12 @@ def main(event, lambda_context):
             if folder not in files:
                 files[folder] = []
             files[folder].append(file)
+            logging.info(f'This is {file} ')
 
     return {
         'statusCode': 200,
         'body': json.dumps({
             "data": files,
+            
         })
     }
