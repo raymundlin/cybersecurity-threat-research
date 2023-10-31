dccf7151f347df8f351228ca15ba58983d3f6d71
John <121889006+jgw0915@users.noreply.github.com>
2023-10-31 06:26:55 +0000

    Add: logging in list.py

--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,8 +1,12 @@
 import os
 import json
+import logging
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
+    if not folder_name :
+        logging.fatal('No folder passed in get_files')
     files = []
     for root, dirs, file_names in os.walk(folder_name):
         for file_name in file_names:
@@ -15,11 +19,14 @@ def main(event, lambda_context):
     files = {}
     folders = ["threat/actor", "domain/study"]
     for folder in folders:
+        logging.info('start getting file in {}'.format(folder))
         for file in get_files(folder):
             if folder not in files:
                 files[folder] = []
             files[folder].append(file)
+            logging.debug('Adding {} to list'.format(file))
 
+    logging.info('returning data')
     return {
         'statusCode': 200,
         'body': json.dumps({
