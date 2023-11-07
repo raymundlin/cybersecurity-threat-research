af5650f86877381a5839360be356443dcfdc8d56
Stan <lol97554@gmail.com>
2023-11-07 14:16:00 +0800

    feat: implement feature toggle with HTML rendering.

--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,5 +1,9 @@
 import os
 import json
+from package.featuretoggles import TogglesList
+
+class ReleaseToggle(TogglesList):
+    renderHTML: bool
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
@@ -9,6 +13,29 @@ def get_files(folder_name):
             if file_name.endswith('.yaml') or file_name.endswith('.yml'):
                 files.append(file_name)
     return files
+
+def renderHTML(files):
+    """Render HTML."""
+    file_list_html = ""
+
+    for folder in files:
+        file_list_html += "<h2>{}</h2>".format(folder)
+        file_list_html += "<ul>"
+        for file in files[folder]:
+            file_list_html += "<li><a href=\"{0}/{1}\">{1}</a></li>".format(folder, file)
+        file_list_html += "</ul>"
+
+    return """
+    <html>
+        <head>
+            <title>Threat Hunting</title>
+        </head>
+        <body>
+            <h1>Threat Hunting</h1>
+            {}
+        </body>
+    </html>
+    """.format(file_list_html)
 
 def main(event, lambda_context):
     """Main function."""
@@ -20,9 +47,14 @@ def main(event, lambda_context):
                 files[folder] = []
             files[folder].append(file)
 
+    toggles = ReleaseToggle('toggle.yaml')
+
     return {
         'statusCode': 200,
-        'body': json.dumps({
+        "headers": {
+            'Content-Type': 'text/html' if toggles.renderHTML else 'application/json',
+        },
+        'body': renderHTML(files) if toggles.renderHTML else json.dumps({
             "data": files,
-        })
+        }),
     }
