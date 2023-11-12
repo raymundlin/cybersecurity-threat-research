a9f7c3f8df5bf9eca0f7cfe39d8aacb54be71437
Raymund Lin <143940096+drlineme@users.noreply.github.com>
2023-11-12 17:22:58 +0800

    flagsmith

--- build_root.py
+++ build_root.py
@@ -1,8 +1,23 @@
 import os
 import pandas as pd
 import logging
+from flagsmith import Flagsmith
 
 logging.basicConfig(filename='build_root.log', encoding='utf-8', level=logging.DEBUG)
+
+flagsmith = Flagsmith(
+    environment_key='ser.UQk4uU3ZMNYcQwsreizvc6',
+    # os.environ['FLAGSMITH_KEY'],
+    enable_local_evaluation=True)
+flagsmith.update_environment()
+flags = flagsmith.get_environment_flags()
+traits = {}
+if os.environ.get('BOT'):
+    traits['bot'] = True
+
+segs = flagsmith.get_identity_segments(identifier="undefined", traits=traits)
+print(segs)
+
 
 def get_files(folder_name):
     files = []
@@ -15,8 +30,13 @@ def get_files(folder_name):
                 logging.debug("find file: {} in folder({})".format(file_name, folder_name))
     return files
 
+
 def get_contents():
-    directory_path = ['domain/study' , 'threat/actor']
+    directory_path = []
+    if flags.is_feature_enabled('domain_info'):
+        directory_path.append('domain/study')
+    if flags.is_feature_enabled('threat_info'):
+        directory_path.append('threat/actor')
     topic_files = {dp: get_files(dp) for dp in directory_path}
     contents = []
     for t in topic_files:
