b21bb16d89c4570a195e9b25cc423a687a3ca4e9
ClC <144094751+b11009061@users.noreply.github.com>
2023-12-19 06:00:38 +0000

    homework

--- deployment/app.py
+++ deployment/app.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python3
 import os
 import shutil
-
 import aws_cdk as cdk
 from deployment.deployment_stack import DeploymentStack
 import os
-
 FOLDERS = ["threat", "domain"]
-
 # Remove app folder assets
 # Copy folders to app folder
 for folder in FOLDERS:
     if os.path.isdir("./app/{}".format(folder)):
       shutil.rmtree("./app/{}".format(folder))
     shutil.copytree("../{}".format(folder), "./app/{}".format(folder))
-
 env = cdk.Environment(account=os.environ["CDK_DEFAULT_ACCOUNT"], region=os.environ["CDK_DEFAULT_REGION"])
 stage = os.environ.get('CDK_DEPLOY_STAGE') or 'dev'
 
 app = cdk.App()
 DeploymentStack(app, "DeploymentStack-{}".format(stage), stage=stage, env=env)
+DeploymentStack(app, "DeploymentStack_B11009045", env=Osaka)
 
 app.synth()
--- deployment/app/list.py
+++ deployment/app/list.py
@@ -1,60 +1,39 @@
 import os
 import json
-from package.featuretoggles import TogglesList
+import logging
 
-class ReleaseToggle(TogglesList):
-    renderHTML: bool
+logging.basicConfig(filename='list.log', level=logging.DEBUG)
+logging.info("Start")
+logging.warning("You have to make sure there is at least one yaml or yml file")
 
 def get_files(folder_name):
     """Get all files in a folder recursively."""
-    files = []
-    for root, dirs, file_names in os.walk(folder_name):
+@@ -8,21 +13,26 @@ def get_files(folder_name):
         for file_name in file_names:
             if file_name.endswith('.yaml') or file_name.endswith('.yml'):
                 files.append(file_name)
+            else:
+                logging.error("There's no yaml or yml file.")
     return files
-
-def renderHTML(files):
-    """Render HTML."""
-    file_list_html = ""
-
-    for folder in files:
-        file_list_html += "<h2>{}</h2>".format(folder)
-        file_list_html += "<ul>"
-        for file in files[folder]:
-            file_list_html += "<li><a href=\"{0}/{1}\">{1}</a></li>".format(folder, file)
-        file_list_html += "</ul>"
-
-    return """
-    <html>
-        <head>
-            <title>Threat Hunting</title>
-        </head>
-        <body>
-            <h1>Threat Hunting</h1>
-            {}
-        </body>
-    </html>
-    """.format(file_list_html)
 
 def main(event, lambda_context):
     """Main function."""
+    logging.debug("Executing Main")
     files = {}
     folders = ["threat/actor", "domain/study"]
+    folders = ["threat/actor", "domain/study", "backup"]
     for folder in folders:
         for file in get_files(folder):
             if folder not in files:
                 files[folder] = []
             files[folder].append(file)
 
-    toggles = ReleaseToggle('toggle.yaml')
+    logging.info("Finished")
 
     return {
         'statusCode': 200,
-        "headers": {
-            'Content-Type': 'text/html' if toggles.renderHTML else 'application/json',
-        },
-        'body': renderHTML(files) if toggles.renderHTML else json.dumps({
+        'body': json.dumps({
             "data": files,
-        }),
+        })
+    }
     }
